f257a2eb636d93f8e6c648dc426f75cc896f96c0
Ldpw0xi1m1d <99180755+Ldpw0xi1m1d@users.noreply.github.com>
2022-02-23 12:02:07 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,17 +4,44 @@
 
 
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+if (value > 1) {
+        for (int j = 2; j < value; j++)
+            if (value % j == 0)
+                return false;
+        return true;
+    } else {
+       return false;
+}
 }
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+int count = 0;
+    if (n == 1) return 2;
+    for (int i = 3; i <= 10000; i += 2) {
+        if (checkPrime(i)) {
+            count++;
+            if (count == (n - 1))
+                return i;
+        }
+    }
 }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+    if (value == 1) return 2;
+    if (value == 2) return 3;
+    if (checkPrime(value) == 1) value += 1;
+    for (int i = value; i <= 10000; i++)
+        if (checkPrime(i)) {
+            return i;
+        }
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+int summa = 0;
+    for (int i = 1; i < hbound; i++)
+        if (checkPrime(i) == 1) {
+            summa = summa + i;
+        }
+    return summa;
 }
+
