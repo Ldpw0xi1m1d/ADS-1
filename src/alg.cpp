6ec111c418a32157d309a98fb9efe5dec9a8a5cf
Ldpw0xi1m1d <99180755+Ldpw0xi1m1d@users.noreply.github.com>
2022-02-23 13:07:41 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,40 +4,41 @@
 
 
 bool checkPrime(uint64_t value) {
-if (value > 1) {
+    if (value > 1) {
         for (int j = 2; j < value; j++)
             if (value % j == 0)
                 return false;
         return true;
-    } else {
-       return false;
-}
+    }
+    else {
+        return false;
+    }
 }
 
 uint64_t nPrime(uint64_t n) {
-int count = 0;
+    int count = 0;
     if (n == 1) return 2;
-    for (int i = 3; i <= 10000; i += 2) {
+    for (int i = 3; ; i += 2) {
         if (checkPrime(i)) {
             count++;
             if (count == (n - 1))
                 return i;
-       }
+        }
     }
-  }
+}
 
 uint64_t nextPrime(uint64_t value) {
     if (value == 1) return 2;
     if (value == 2) return 3;
     if (checkPrime(value) == 1) value += 1;
-    for (int i = value; i <= 10000; i++)
+    for (int i = value; ; i++)
         if (checkPrime(i)) {
             return i;
-    }
-  }
+        }
+}
 
 uint64_t sumPrime(uint64_t hbound) {
-int summa = 0;
+    int summa = 0;
     for (int i = 1; i < hbound; i++)
         if (checkPrime(i) == 1) {
             summa = summa + i;
